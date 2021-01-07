3053af18f29f45f813481d45c23b6731efbaf945
wangjian <1213847094@qq.com>
2021-01-07 13:20:01 +0800

    no message

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     'hostname'        => '192.168.1.32',
 
     // 数据库名
-    'database'        => 'zentao',
+    'database'        => 'root',
     // 用户名
     'username'        => 'root',
     // 密码
